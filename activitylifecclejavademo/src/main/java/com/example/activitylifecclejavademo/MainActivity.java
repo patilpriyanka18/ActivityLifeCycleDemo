385c2501a9f76f50f4c02d5c06974bc8d9633a16
Priyanka <priyankapatil1819@gmail.com>
2018-04-11 07:43:46 +0530

    Activity Life Cycle Demo

---
+++ activitylifecclejavademo/src/main/java/com/example/activitylifecclejavademo/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.activitylifecclejavademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mt("OnCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mt("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mt("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        mt("onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        mt("OnStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mt("OnDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        mt("OnRestar");
+    }
+    private void mt(String msg){
+        Toast.makeText(this,"",Toast.LENGTH_SHORT).show();
+    }
+
+}
---
+++ app/src/main/java/com/example/pc/activitylifecycle/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.pc.activitylifecycle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+    private fun mt(msg : String) = Toast
+            .makeText(this@MainActivity, msg, Toast.LENGTH_SHORT)
+            .show()
+}
